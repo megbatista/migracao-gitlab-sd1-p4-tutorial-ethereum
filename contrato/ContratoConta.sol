3351286d5eccc7b890d93373109ab7a0e48e01d3
Meg Batista <meg1489@hotmail.com>
2017-12-09 20:44:20 +0000

    Update CustodialContrato.sol

---
+++ contrato/ContratoConta.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.4.0;
+
+contract ContratoConta {
+
+   address cliente;
+   // variavel booleana que facilita
+   // a verificação
+   bool _switch = false;
+   
+   // eventos que mandam mensagens de atualização
+   // ao usuario
+   event AtualizaStatus(string _msg);
+   event StatusUsuario(string _msg, address user, uint amount);
+   
+   // construtor 
+   function ContratoConta() public {
+       // atribui o dono da conta à variavel cliente
+       cliente = msg.sender;
+   }
+   
+   // modificdador usado para reaproveitar codigo
+   modifier ifCliente() {
+       if(msg.sender != cliente) {
+           throw;
+       }else {
+            // retorna para a função que chamou o modificador
+            _;
+       }
+   }
+   
+   // O payable indica que essa é uma função
+   // que movimenta valores
+   function depositaFundos() payable public{
+       StatusUsuario("Usuario transferiu dinheiro", msg.sender, msg.value);
+   }
+   
+   function verificaFundos(uint amount) ifCliente public{
+       // verifica se o dinheiro foi transferido
+       if(cliente.send(amount)){
+           AtualizaStatus("Usuario transferiu dinheiro");
+           // indica que a transferencia foi feita com sucesso
+           _switch = true;
+       }else {
+           _switch = false;
+       }
+   }
+   
+   // mostra a quantia de dinheiro na conta para o usuario
+   // o uso do modificador ifCliente permite que somente
+   // o dono da conta tenha acesso
+   function getFundos() public ifCliente constant returns(uint){
+       return this.balance;
+   }
+   
+}
